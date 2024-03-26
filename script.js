7571f7b82ea74c8b941cad719b8b60deeb78a35f
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-26 19:31:01 +0200

    alert if there is not a name entered

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ function onCityFormSubmit(event) {
 
 	const cityInput = cityForm.querySelector(`#city`);
 	const cityName = cityInput.value.trim();
+
+	if (!cityName) {
+		alert(`Introduceti numele unui oras`);
+		return;
+	}
+	console.log(cityName);
 }
