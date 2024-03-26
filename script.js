ac4fcedf99ab90771046a1009d53cbb85f065523
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-26 19:30:20 +0200

    trim spaces on city inp[uts

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ cityForm.addEventListener(`submit`, onCityFormSubmit);
 
 function onCityFormSubmit(event) {
 	event.preventDefault();
+
+	const cityInput = cityForm.querySelector(`#city`);
+	const cityName = cityInput.value.trim();
 }
